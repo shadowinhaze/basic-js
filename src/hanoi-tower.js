64e007da8a5b51c04e2e0e74c155a9526981073d
shadowinhaze <shadowinhaze@gmail.com>
2021-09-29 15:41:23 +0300

    feat: add hanoi resolution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+  const answer = {
+    'turns': 0,
+    'seconds': 0
+  }
+  answer.turns = Math.pow(2, disksNumber) - 1;
+  answer.seconds = Math.floor(answer.turns / (turnsSpeed / 3600));
+  return answer;
 }
