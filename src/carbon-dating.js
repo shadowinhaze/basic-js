da44cfa82164d567c9a241e7c32f2b65df5ddf42
shadowinhaze <shadowinhaze@gmail.com>
2021-09-29 03:02:56 +0300

    feat: add carbon dating resolution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,9 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (typeof sampleActivity == 'string' && /^\d{1,}[.]{0,1}\d{0,}$/.test(sampleActivity) && sampleActivity != NaN && sampleActivity != Infinity && +sampleActivity <= 15 && +sampleActivity !== 0 ) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
+  }
+  return false;
 }
