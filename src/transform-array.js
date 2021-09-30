8ab00c96f61b6e6230a2d6f17be70f30161783d9
shadowinhaze <shadowinhaze@gmail.com>
2021-09-30 17:05:27 +0300

    feat: resolution for st-sum-digits

--- src/transform-array.js
+++ src/transform-array.js
@@ -16,4 +16,37 @@ import { NotImplementedError } from '../extensions/index.js';
 export default function transform(/* arr */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
+  // let newArr = arr.splice();
+
+  // newArr.forEach((item, index, array) => {
+  //     if (typeof item === 'string') {
+  //         if (array.includes('--discard-next') && array.includes('--double-prev')) {
+  //             return newArr.splice(index,3)
+  //         }
+  //         if (array.includes('--discard-next') && array.includes('--discard-prev')) {
+  //             return newArr.splice(index,3)
+  //         }
+  //         if (item.includes('prev') && index == 0) {
+  //             return newArr.splice(0,1)
+  //         }
+  //         if (item.includes('next') && index == array.length - 1) {
+  //             return newArr.splice(index,1)
+  //         }
+  //         if (item.includes('double-next')) {
+  //             return newArr.splice(index, 1, array[index+1])
+  //         }
+  //         if (item.includes('double-prev')) {
+  //             return newArr.splice(index, 1, array[index-1])
+  //         }
+  //         if (item.includes('discard-next')) {
+  //             return newArr.splice(index, 2)
+  //         }
+  //         if (item.includes('discard-prev')) {
+  //             return newArr.splice(index-1, 2)
+  //         }
+  //     }
+  // })
+
+  // return newArr;
 }
