09815e5d6cd630d0103643a9e8f9d2a1c7a908a0
shadowinhaze <shadowinhaze@gmail.com>
2021-10-01 15:11:05 +0300

    feat: add commin-character-count solution

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1,s2) {
+  function encoder(str) {
+    let code = {};
+    [...str].forEach(letter => {
+        if (code.hasOwnProperty(letter)) {
+            code[letter] += 1;
+        } else {
+            code[letter] = 1;
+        }
+    })
+    return Object.entries(code)
+  }
+  let counter = 0;
+  encoder(s1).forEach(letterBlock1 => {
+      encoder(s2).forEach(letterBlock2 => {
+          if (letterBlock1[0] === letterBlock2[0]) {
+              counter += Math.min(letterBlock1[1], letterBlock2[1])
+          }
+      })
+  })
+  return counter;
 }
