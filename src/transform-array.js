5429404e94a2812f7ce44eab989d69dcb3452618
shadowinhaze <shadowinhaze@gmail.com>
2021-10-02 21:53:08 +0300

    refactor: add more simple solution for transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,45 +14,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function transform(arr) {
-  // throw new NotImplementedError('Not implemented');
-  // // remove line with error and write your code here
   if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
-  let newArr = [...arr];
-  newArr.forEach((item, index, array) => {
-      if (typeof item === 'string') {
-          switch (true) {
-              case item.includes('prev') && index == 0:
-                  newArr.splice(0, 1)
-                  break;
-              case item.includes('next') && index == array.length - 1:
-                  newArr.splice(index, 1)
-                  break;
-              case item.includes('--discard-next') && array.includes('--double-prev'):
-                  newArr.splice(index, 3)
-                  break;
-              case item.includes('--discard-next') && array.includes('--discard-prev'):
-                  newArr.splice(index, 3)
-                  break;
-              case item.includes('--double-next') && array.includes('--double-prev'):
-                  newArr.splice(index, 3, array[index +1], array[index +1], array[index +1])
-                  break;
-              case item.includes('--double-next') && array.includes('--discard-prev'):
-                  newArr.splice(index, 3, array[index +1])
-                  break;
-              case item.includes('double-next'):
-                  newArr.splice(index, 1, array[index + 1])
-                  break;
-              case item.includes('double-prev'):
-                  newArr.splice(index, 1, array[index - 1])
-                  break;
-              case item.includes('discard-next'):
-                  newArr.splice(index, 2)
-                  break;
-              case item.includes('discard-prev'):
-                  newArr.splice(index - 1, 2)
-                  break;
-          }
-      }
-  })
-  return newArr;
+    let newArr = [...arr].flatMap((item, i, aR) => {
+        switch (true) {
+            case (aR[i - 1] === '--double-next' && aR[i + 1] === '--double-prev'):
+                return [item, item, item];
+            case (aR[i - 1] === '--double-next' && aR[i + 1] === '--discard-prev'):
+                return [item];
+            case (aR[i + 1] === '--discard-prev'):
+                 return [];
+            case (aR[i - 1] === '--discard-next'):
+                 return [];
+            case (aR[i + 1] === '--double-prev'):
+                return [item, item];
+            case (aR[i - 1] === '--double-next'):
+                return [item, item];
+            case item.toString().startsWith('--d'):
+                return [];
+        }
+        return item;
+    });
+    return newArr;
 }
